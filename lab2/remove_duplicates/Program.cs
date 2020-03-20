4bab16fa892ed4dd05e81d7300e55466372a55ee
Alyona Semyonova <alien.semy@gmail.com>
2020-03-20 14:45:58 +0300

    remove_duplicates

---
+++ lab2/remove_duplicates/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace remove_duplicates
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No parameters were specified!");
+                return 1;
+            }
+            if (args.Length > 1)
+            {
+                Console.WriteLine("Incorrect number of arguments!");
+                Console.WriteLine("Usage remove_duplicates.exe <input string>");
+                return 1;
+            }
+            string newString = args[0];
+            for (int i = 0; i < newString.Length; i++)
+            {
+                int j;
+                for (j = i + 1; j < newString.Length; j++)
+                {
+                    if (newString[i] == newString[j])
+                    {
+                        newString = newString.Remove(j, 1);
+                    }
+                }
+            }
+            Console.WriteLine(newString);
+            return 0;
+        }
+    }
+}
