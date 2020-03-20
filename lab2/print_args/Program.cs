b0d4e2881315879f6df95b47c57802428cd1839a
Alyona Semyonova <alien.semy@gmail.com>
2020-03-20 14:43:14 +0300

    print_args

---
+++ lab2/print_args/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace print_args
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No parameters were specified!");
+                return 1;
+            }
+            Console.WriteLine("Number of arguments: " + args.Length);
+            Console.WriteLine("Arguments: " + String.Join(" ", args));
+            return 0;
+        }
+    }
+}
