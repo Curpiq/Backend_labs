a5971cf0fa85051194c241fedb586a4dda6ec25e
Alyona Semyonova <alien.semy@gmail.com>
2020-03-28 14:29:17 +0300

    CheckIdentifier

---
+++ lab3/CheckIdentifier/CheckIdentifier/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckIdentifier
+{
+    class Program
+    {
+        static bool IsLetter(char ch)
+        {
+            return (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch >= 'А' && ch <= 'Я') ||
+                (ch >= 'a' && ch <= 'я') || (ch == 'Ё') && (ch == 'ё');
+        }
+        static bool IsDigit(char digit)
+        {
+            return (digit >= '0' && digit <= '9');
+        }
+        static int Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("No");
+                Console.WriteLine("Invalid arguments count");
+                Console.WriteLine("Usage: CheckIdentifier.exe <identifier>");
+                return 1;
+            }
+            string inputString = args[0];
+            if (String.IsNullOrEmpty(inputString))
+            {
+                Console.WriteLine("No");
+                Console.WriteLine("The string must not be empty");
+                return 1;
+            }
+            if (IsDigit(inputString[0]))
+            {
+                Console.WriteLine("No");
+                Console.WriteLine("Identifier must be start with letter");
+                return 1;
+            }
+            for (int i = 1; i < inputString.Length; i++)
+            {
+                if (!IsDigit(inputString[i]) && !IsLetter(inputString[i]))
+                {
+                    Console.WriteLine("No");
+                    Console.WriteLine("Identifier must be contain only letters or digits");
+                    return 1;
+                }
+            }
+            Console.WriteLine("Yes");
+            return 0;
+        }
+    }
+}
