3bc1ca236211f32195d1e67344bbce8200fc1d7e
Alyona Semyonova <alien.semy@gmail.com>
2020-03-28 23:14:31 +0300

    Unit tests

---
+++ lab3/CheckIdentifier/CheckIdentifierTests/CheckIdentifierTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CheckIdentifier;
+namespace CheckIdentifierTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void BeginWithDigit()
+        {
+            string[] wrong = new string[1] { "5ABC" };
+            int result = Program.Main(wrong);
+            Assert.AreEqual(1, result);
+        }
+        [TestMethod]
+        public void IncorrectSymbol()
+        {
+            string[] wrong = new string[1] { "dkl1#" };
+            int result = Program.Main(wrong);
+            Assert.AreEqual(1, result);
+        }
+        [TestMethod]
+        public void CorrectArg()
+        {
+            string[] wrong = new string[1] { "sDN12V5" };
+            int result = Program.Main(wrong);
+            Assert.AreEqual(0, result);
+        }
+        [TestMethod]
+        public void ManyArguments()
+        {
+            string[] wrong = new string[2] { "1", "2" };
+            int result = Program.Main(wrong);
+            Assert.AreEqual(1, result);
+        }
+        [TestMethod]
+        public void FewArguments()
+        {
+            string[] wrong = new string[0] { };
+            int result = Program.Main(wrong);
+            Assert.AreEqual(1, result);
+        }
+        [TestMethod]
+        public void IncorrectLetter()
+        {
+            string[] wrong = new string[1] { "VF1ялн" };
+            int result = Program.Main(wrong);
+            Assert.AreEqual(1, result);
+        }
+        [TestMethod]
+        public void EmptyString()
+        {
+            string[] wrong = new string[1] { "" };
+            int result = Program.Main(wrong);
+            Assert.AreEqual(1, result);
+        }
+    }
+}
