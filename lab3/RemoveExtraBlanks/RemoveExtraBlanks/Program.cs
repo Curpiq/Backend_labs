a2ea99c23fa0fa6d798b307e185def67b7f1afcf
Alyona Semyonova <alien.semy@gmail.com>
2020-03-30 14:52:00 +0300

    RemoveExtraBlanks

---
+++ lab3/RemoveExtraBlanks/RemoveExtraBlanks/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveExtraBlanks
+{
+
+    public class Program
+    {
+        public static bool ParseArgs(string[] args, ref string inputFile, ref string outputFile)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Invalid arguments count");
+                Console.WriteLine("Usage: RemoveExtraBlanks.exe <input file> <output file>");
+                return false;
+            }
+            inputFile = args[0];
+            outputFile = args[1];
+            return true;
+        }
+
+        public static string RemoveExtraBlanksInLine(string line)
+        {
+            string newline = line.Trim();
+            newline = RemoveRepetitiveBlanksInLine(newline);
+            return newline;
+
+        }
+        public static string RemoveRepetitiveBlanksInLine(string line)
+        {
+            string newLine = "";
+            bool flag = false;
+            for (int i = 0; i < line.Length; i++)
+            {
+                if ((line[i] == ' ') || (line[i] == '\t'))
+                {
+                    if (!flag)
+                    {
+                        newLine = newLine + line[i];
+                    }
+                    flag = true;
+                }
+                else
+                {
+                    newLine = newLine + line[i];
+                    flag = false;
+                }
+            }
+            return newLine;
+        }
+
+        public static bool RemoveExtraBlanksInFile(string inputFile, string outputFile)
+        {
+            if (!File.Exists(inputFile) || !File.Exists(outputFile))
+            {
+                Console.WriteLine(inputFile + " and/or " + outputFile + " doesn't exists");
+                return false;
+            }
+            StreamReader input = new StreamReader(inputFile);
+            StreamWriter output = new StreamWriter(outputFile);
+            string line;
+            while ((line = input.ReadLine()) != null)
+            {
+                string newLine = RemoveExtraBlanksInLine(line);
+                output.WriteLine(newLine);
+            }
+            input.Close();
+            output.Close();
+            return true;
+        }
+
+        public static int Main(string[] args)
+        {
+            string inputFile = "";
+            string outputFile = "";
+            if (!ParseArgs(args, ref inputFile, ref outputFile))
+            {
+                return 1;
+            }
+            if (!RemoveExtraBlanksInFile(inputFile, outputFile))
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
---
+++ lab3/RemoveExtraBlanks/RemoveExtraBlanksTests/RemoveExtraBlanks_Tests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RemoveExtraBlanks;
+
+namespace RemoveExtraBlanksTests
+{
+    [TestClass]
+    public class RemoveExtraBlanks_Tests
+    {
+        [TestMethod]
+        public void ParseArgs_NoArguments_FalseReturned()
+        {
+            string[] wrong = new string[1] { "" };
+            string fileName = "";
+            bool result = Program.ParseArgs(wrong, ref fileName, ref fileName);
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        public void ParseArgs_ManyArguments_FalseReturned()
+        {
+            string[] wrong = new string[3] { "1", "2", "3" };
+            string fileName = "";
+            bool result = Program.ParseArgs(wrong, ref fileName, ref fileName);
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        public void ParseArgs_FewArguments_FalseReturned()
+        {
+            string[] wrong = new string[1] { "1" };
+            string fileName = "";
+            bool result = Program.ParseArgs(wrong, ref fileName, ref fileName);
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        public void ParseArgs_CorrectArgsCount_TrueReturned()
+        {
+            string[] correct = new string[2] { "input.txt", "output.txt" };
+            string fileName = "";
+            bool result = Program.ParseArgs(correct, ref fileName, ref fileName);
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void RemoveRepetitiveBlanksInLine_Test()
+        {
+            string str = " Hello    world     ! ";
+            string expected = " Hello world ! ";
+            string result = Program.RemoveRepetitiveBlanksInLine(str);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void RemoveExtraBlanksInLine_Line_With_Blanks_In_The_Beginning_And_End()
+        {
+            string str = " Hello    world     ! ";
+            string expected = "Hello world !";
+            string result = Program.RemoveExtraBlanksInLine(str);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
